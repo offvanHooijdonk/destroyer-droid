4e5b320aa656d2dcaa77ebb40f19e8c1e7aa0364
Yahor Fralou <Yahor_Fralou@epam.com>
2017-03-17 12:34:07 +0300

    Initial commit

---
+++ app/src/main/java/by/vanhooijdonk/destroyerdroid/MainActivity.java
@@ -0,0 +1,15 @@
+package by.vanhooijdonk.destroyerdroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import destroyerdroid.vanhooijdonk.by.destroyer_droid.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
